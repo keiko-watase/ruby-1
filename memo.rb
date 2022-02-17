c979e99be8568a12c0a931a516e3475b915a037e
keiko watase <keikowatase@keikonoMacBook-Pro.local>
2022-02-17 22:18:16 +0900

    複数行入力できるように修正しました

--- memo.rb
+++ memo.rb
@@ -11,20 +11,20 @@ if memo_type == 1
   file_title = gets.chomp
   p "メモしたい内容を入力してください"
   p "完了したらCtrl + Dを押します"
-  memo_contents = gets.chomp
-  CSV.open("#{file_title}.csv",'w') do |memo|
+  memo_contents = readlines
+  CSV.open("#{file_title}.csv","w") do |memo|
     memo << ["memo"]
-    memo << [memo_contents]
-end
+    memo << memo_contents
+  end
 elsif memo_type == 2
   puts "編集したいファイル名を入力してください"
   file_title = gets.chomp
   p "編集内容を入力してください"
   p "完了したらCtrl + Dを押します"
-  memo_contents = gets.chomp
-  CSV.open("#{file_title}.csv",'a') do |memo|
-    memo << [memo_contents]
-end
+  memo_contents = readlines
+  CSV.open("#{file_title}.csv","a") do |memo|
+    memo << memo_contents
+  end
 else
   puts "エラー"
 end
