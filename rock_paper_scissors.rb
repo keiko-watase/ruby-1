7969c587eb62b822d0da3459492c30548fc57a7b
keiko watase <keikowatase@keikonoMacBook-Pro.local>
2022-02-20 10:24:38 +0900

    ruby実践課題(じゃんけんアプリ)の提出

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,8 +1,7 @@
-
-def rock_paper_scissors()
+def rock_paper_scissors
   puts "じゃんけん..."
   puts "0(グー)1(チョキ)2(パー)3(戦わない)"
-
+  
   me = gets.to_i
 
   if me == 0
@@ -12,15 +11,15 @@ def rock_paper_scissors()
   elsif me == 2
     my_type = "パー"
   elsif me == 3
-    puts "--------------------"
+    puts "--------------"
     puts "あなた:戦いません"
-    puts "相手:また勝負しましょう"
-    puts "--------------------"
+    puts "相手:また戦いましょう"
+    puts "--------------"
     exit
   else
     until me == 0 || me == 1 || me == 2 || me == 3 do
-      puts "0~3を入力してください"
-      me = gets.to_i
+        puts "0~3を入力してください"
+        me = gets.to_i
     end
     if me == 0
       my_type = "グー"
@@ -29,30 +28,29 @@ def rock_paper_scissors()
     elsif me == 2
       my_type = "パー"
     elsif me == 3
-      puts "--------------------"
+      puts "--------------"
       puts "あなた:戦いません"
-      puts "相手:また勝負しましょう"
-      puts "--------------------"
+      puts "相手:また戦いましょう"
+      puts "--------------"
       exit
     end
   end
 
   you = ["グー","チョキ","パー"]
   your_type = you.sample
-  
+
   puts "ホイ!"
-  puts "--------------------"
+  puts "--------------"
   puts "あなた:#{my_type}を出しました"
   puts "相手:#{your_type}を出しました"
-  puts "--------------------"
+  puts "--------------"
 
   while my_type == your_type do
-
     puts "あいこで..."
     puts "0(グー)1(チョキ)2(パー)3(戦わない)"
 
     me = gets.to_i
-
+    
     if me == 0
       my_type = "グー"
     elsif me == 1
@@ -60,10 +58,10 @@ def rock_paper_scissors()
     elsif me == 2
       my_type = "パー"
     elsif me == 3
-      puts "--------------------"
+      puts "--------------"
       puts "あなた:戦いません"
-      puts "相手:また勝負しましょう"
-      puts "--------------------"
+      puts "相手:また戦いましょう"
+      puts "--------------"
       exit
     else
       until me == 0 || me == 1 || me == 2 || me == 3 do
@@ -71,17 +69,16 @@ def rock_paper_scissors()
         me = gets.to_i
       end
       if me == 0
-        my_type = "グー"
+         my_type = "グー"
       elsif me == 1
         my_type = "チョキ"
       elsif me == 2
         my_type = "パー"
       elsif me == 3
-        puts "--------------------"
+        puts "--------------"
         puts "あなた:戦いません"
-        puts "相手:また勝負しましょう"
-        puts "--------------------"
-        exit
+        puts "相手:また戦いましょう"
+        puts "--------------"
       end
     end
 
@@ -89,12 +86,13 @@ def rock_paper_scissors()
     your_type = you.sample
 
     puts "ショ!"
-    puts "--------------------"
+    puts "--------------"
     puts "あなた:#{my_type}を出しました"
     puts "相手:#{your_type}を出しました"
-    puts "--------------------"
+    puts "--------------"
 
   end
+  
 
   if (my_type == "グー" && your_type == "チョキ") || (my_type == "チョキ" && your_type == "パー") || (my_type == "パー" && your_type == "グー")
     $game_result1 = 1
@@ -104,8 +102,7 @@ def rock_paper_scissors()
 
 end
 
-def facing_overthere()
-
+def facing_overthere
   puts "あっち向いて〜"
   puts "0(上)1(下)2(左)3(右)"
 
@@ -121,8 +118,8 @@ def facing_overthere()
     my_direction = "右"
   else
     until me == 0 || me == 1 || me == 2 || me == 3 do
-      puts "0~3を入力してください"
-      me = gets.to_i
+        puts "0~3を入力してください"
+        me = gets.to_i
     end
     if me == 0
       my_direction = "上"
@@ -134,25 +131,26 @@ def facing_overthere()
       my_direction = "右"
     end
   end
-  
+
   you = ["上","下","左","右"]
   your_direction = you.sample
-    
+
   puts "ホイ!"
-  puts "--------------------"
+  puts "--------------"
   puts "あなた:#{my_direction}"
   puts "相手:#{your_direction}"
-  puts "--------------------"  
+  puts "--------------"
 
   if my_direction == your_direction
     $game_result2 = 1
   else
     $game_result2 = 2
   end
+
 end
 
-rock_paper_scissors()
-facing_overthere()
+rock_paper_scissors
+facing_overthere
 
 if $game_result2 == 1
   if $game_result1 == 1
@@ -162,13 +160,14 @@ if $game_result2 == 1
   end
 else
   while $game_result2 == 2 do
-   rock_paper_scissors()
-   facing_overthere()
+    rock_paper_scissors
+    facing_overthere
   end
-  if $game_result1 == 1
-    puts "あなたの勝ち"
-  else
-    puts "相手の勝ち"
-  end
+    if $game_result1 == 1
+      puts "あなたの勝ち"
+    else
+      puts "相手の勝ち"
+    end
 end
-    
+
+  
